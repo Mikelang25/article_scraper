d5a913f953c11945f1b1f176c86c2ac003f09038
Mike Lang <michaellang2525@yahoo.com>
2020-03-06 18:24:07 -0500

    updated port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const db = require("./models");
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
